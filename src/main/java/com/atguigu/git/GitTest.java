5612854379304c05230c6d1486fc3342072e7c09
Xyj <1779308193@qq.com>
2024-05-02 20:30:08 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2222");
         System.out.println("hello git3333");
+        System.out.println("hello git4444");
     }
 }
